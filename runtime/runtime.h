hopez13/art

Add visiting for class loaders in StickyMarkSweep
StickyMarkSweep clear the mark stack,
Because all reachable objects must be referenced by a root or a dirty card.
But, there are some marking hole for class object.

If some object is marked and the object and it's class object is not dirty,
Then class object cannot be marking by card table.

In previous OS including mashmellow,
Class table was maintaned by class linker
and all class object was marked with kVisitRootFlagAllRoots flag.

In N OS,
Class object is not marked with kVisitRootFlagAllRoots.
So, I added new flag to mark class object and using it StickyMarkSweep.

Test: none

Change-Id: I57599e6db53b260f4c5ef466b63962141b8da5c3
Signed-off-by: Hyangseok Chae <neo.chae@lge.com>

runtime/runtime.h
@@ -110,6 +110,7 @@ enum VisitRootFlags : uint8_t {
   // Non moving means we can have optimizations where we don't visit some roots if they are
   // definitely reachable from another location. E.g. ArtMethod and ArtField roots.
   kVisitRootFlagNonMoving = 0x20,
+  kVisitRootFlagClassLoader = 0x40,
 };
 
 class Runtime {

SlimRoms/android_art

Added regression test in 141-class-unload.
Test: test-art-host
Merged-In: I57599e6db53b260f4c5ef466b63962141b8da5c3
(cherry picked from commit a2d1b28599e38ee0180f0f7130a879eac5be9dec)
Bug: 33924225

runtime/runtime.h
@@ -105,9 +105,7 @@ enum VisitRootFlags : uint8_t {
   kVisitRootFlagStartLoggingNewRoots = 0x4,
   kVisitRootFlagStopLoggingNewRoots = 0x8,
   kVisitRootFlagClearRootLog = 0x10,
-  // Non moving means we can have optimizations where we don't visit some roots if they are
-  // definitely reachable from another location. E.g. ArtMethod and ArtField roots.
-  kVisitRootFlagNonMoving = 0x20,
+  kVisitRootFlagClassLoader = 0x20,
 };
 
 class Runtime {